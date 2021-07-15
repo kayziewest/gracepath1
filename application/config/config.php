319478e0950875954a730962bbc9755624a68f3f
oy3 <46595215+oy3@users.noreply.github.com>
2021-07-15 11:08:25 +0100

    Changed base_url
    Changed the base_url from localhost to Heroku's URL

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/hms/';
+$config['base_url']	= 'https://gracepath1.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
